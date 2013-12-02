b4ad70fdfd1461632bffb5860ba79c654e3c138c
Marcus Willock <crazcalm@gmail.com>
2013-12-01 23:24:28 -0500

    NEVER TRY TO COMMIT A FILE THAT IS MORE THAT 100 MB

--- practice.py
+++ practice.py
@@ -7,8 +7,15 @@ def parse_html_table(file_name):
     
     stack_2d = []                        # A list that will hold tuple of size 3
     inner_stack = []
-    
-    for string in tbody_tag[3].strings:  # Lets me interate over the string in the html
+
+    for i, value in enumerate(tbody_tag):
+        #print i, len(value)
+        if len(value)> 20:
+			index = i
+
+    #print tbody_tag[index]
+   
+    for string in tbody_tag[index].strings:  # Lets me interate over the string in the html
         
         inner_stack.append(string)    
         
@@ -18,8 +25,26 @@ def parse_html_table(file_name):
             
     return stack_2d                             #note: string are in Unicode...
 
+def data_parser(data):
+    """
+    This is a sample of a parser we could use.
+    The indexes between the html list and the data list
+    seem to be off by 1.
+    """
+
+    file = open(data, "r").readlines(1024)     #my small computer cant handle the entire file at once...
+
+    for index, line in enumerate(file):
+        file[index] = line.split("|")
+
+    return file
+
 if __name__=="__main__":
     test =  parse_html_table("Report.html")
     for index, value in enumerate(test):
         print index, value
+        raw_input()
 
+    test2 = data_parser("hudson")
+    for index, value in enumerate(test2[0]):
+        print index + 1, value
