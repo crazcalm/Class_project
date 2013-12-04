c1cb7503a7e85a3540200dd031eb63458e1895a9
Marcus Willock <crazcalm@gmail.com>
2013-12-03 20:34:38 -0500

    Starting to match up the tags with the info

--- practice.py
+++ practice.py
@@ -1,5 +1,16 @@
 from bs4 import BeautifulSoup             # A web scraping tool
 
+def main():
+	
+	tags = parse_html_table("Report.html")	
+
+	for count, item in enumerate(data_parser("hudson")):
+		for index,value in enumerate(item[:-1]):
+			print "%s: %s" % (tags[index + 1][0], value)
+		if count ==1:
+			break 
+		
+
 def parse_html_table(file_name):
     file = open(file_name, "r")
     soup = BeautifulSoup(file)           # creating a bs4 object
@@ -31,20 +42,33 @@ def data_parser(data):
     The indexes between the html list and the data list
     seem to be off by 1.
     """
+    file = open(data, "r")
+    tempt = "blah, blah, blah"
+    while tempt != '':
+        tempt = file.readline()
+        yield tempt[:-1].split("|")
 
+    print "!!!!!!!!!!!!!!!!STOPPED!!!!!!!!!!!!!!!!!"
+
+    """
     file = open(data, "r").readlines(1024)     #my small computer cant handle the entire file at once...
 
     for index, line in enumerate(file):
         file[index] = line.split("|")
-
-    return file
+    """
+    #return file
 
 if __name__=="__main__":
+    """
     test =  parse_html_table("Report.html")
     for index, value in enumerate(test):
         print index, value
         raw_input()
 
     test2 = data_parser("hudson")
-    for index, value in enumerate(test2[0]):
+    for index, value in enumerate(test2):
         print index + 1, value
+        if index > 3:
+            break
+    """
+    main()
